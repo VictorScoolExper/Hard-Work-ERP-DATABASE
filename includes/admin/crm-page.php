9e7137cd8449e59208d2e695d90efa1b38523b2c
VictorScoolExper <94493104+VictorScoolExper@users.noreply.github.com>
2023-02-18 15:17:49 -0800

    added boostrap

--- includes/admin/crm-page.php
+++ includes/admin/crm-page.php
@@ -1,5 +1,27 @@
 <?php 
 
 function hw_erp_crm_page(){
-    ?> <div class="wrap">Hello from CRM PAGE</div> <?php
+    ?>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">CRM</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Dashboard</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Contact</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Settings</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="wrap">Hello from CRM PAGE</div> <?php
 }
--- includes/admin/dashboard-page.php
+++ includes/admin/dashboard-page.php
@@ -3,6 +3,7 @@
 function hw_erp_dashboard_page(){
     ?>
     <div class="wrap">
+        
         <h1><?php esc_html_e('Hard Work ERP Dashboard', 'hard-work-erp' ); ?></h1>
     </div>
     <?php
