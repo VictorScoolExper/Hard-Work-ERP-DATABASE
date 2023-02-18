1376bb3028371b04d1574efef2faf24b2b9a5b13
VictorScoolExper <victorscool477@outlook.com>
2023-02-18 10:47:36 -0600

    modified db tables

--- db_tables/tables_hard_Work.sql
+++ db_tables/tables_hard_Work.sql
@@ -50,7 +50,7 @@ CREATE TABLE hw_erp_category_service(
 CREATE TABLE hw_erp_services (
   service_id BIGINT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100) NOT NULL,
-  per_hour TINYINT NOT NULL,
+  is_per_hour TINYINT NOT NULL,
   price DECIMAL(15,2) NOT NULL,
   category_id  BIGINT NOT NULL,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -81,7 +81,7 @@ CREATE TABLE hw_erp_job_tasks (
 CREATE TABLE hw_erp_crews (
   crew_id BIGINT AUTO_INCREMENT PRIMARY KEY,
   crew_name VARCHAR(255) NOT NULL,
-  schedule_check_time DATETIME NOT NULL,
+  schedule_check_time DATETIME,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 );
@@ -108,13 +108,14 @@ CREATE TABLE hw_erp_employees (
 -- this table associates jobs to crews and allows a crew to have multiple jobs a day
 --  with the corresponding tasks
 CREATE TABLE hw_erp_crew_jobs(
-    work_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    crew_job_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     crew_id BIGINT NOT NULL,
     address_id BIGINT NOT NULL,
     client_id BIGINT NOT NULL,
     job_id BIGINT NOT NULL,
     work_description TEXT NOT NULL,
-    notes TEXT,
+    description TEXT NOT NULL,
+    status ENUM('TO DO', 'IN PROGRESS', 'DONE' 'Canceled') NOT NULL DEFAULT 'TO DO',
     start_date DATE NOT NULL,
     end_date DATE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -125,17 +126,17 @@ CREATE TABLE hw_erp_crew_jobs(
     FOREIGN KEY (job_id) REFERENCES hw_erp_jobs(job_id)
 );
 
-CREATE TABLE hw_erp_list_work_activities(
-    work_activity_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    work_id BIGINT NOT NULL,
-    description TEXT NOT NULL,
-    status ENUM('TO DO', 'IN PROGRESS', 'DONE') NOT NULL DEFAULT 'TO DO',
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (work_id) REFERENCES hw_erp_work_schedule(work_id)
+CREATE TABLE hw_erp_job_reports(
+  job_report_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  crew_job_id BIGINT NOT NULL,
+  comments TEXT,
+  inconveniences TEXT,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (crew_job_id) REFERENCES hw_erp_crew_jobs(crew_job_id)
 );
 
-CREATE TABLE hw_erp_equipment_brands(
+CREATE TABLE hw_erp_brands(
     brand_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     brand_name VARCHAR(200) NOT NULL,
     brand_img VARCHAR(500), 
@@ -145,70 +146,73 @@ CREATE TABLE hw_erp_equipment_brands(
 
 CREATE TABLE hw_erp_equipment_categories(
     equipment_category_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    main_category VARCHAR(200),
+    main_category VARCHAR(200) NOT NULL,
     sub_category VARCHAR(200),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
-CREATE TABLE hw_erp_equipment_info (
-  equipment_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  equipment_name VARCHAR(255) NOT NULL,
-  equipment_description TEXT NOT NULL,
-  equipment_brand BIGINT NOT NULL,
-  equipment_price DECIMAL(10,2)
-  equipment_category_id BIGINT NOT NULL,
+CREATE TABLE hw_erp_equipment_stock (
+  equip_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT NOT NULL,
+  vendor_id BIGINT NOT NULL,
+  brand_id BIGINT NOT NULL,
+  equip_category_id BIGINT NOT NULL,
+  last_maintenance_date date NOT NULL,
+  status ENUM('AVAILABLE', 'IN_USE', 'MAINTENANCE') NOT NULL DEFAULT 'AVAILABLE',
+  total DECIMAL(10,2),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (equipment_brand) REFERENCES hw_erp_equipment_brands(brand_id),
-  FOREIGN KEY (equipment_category_id) REFERENCES hw_erp_equipment_categories(equipment_category_id)
+  FOREIGN KEY (vendor_id) REFERENCES hw_erp_vendor(vendor_id),
+  FOREIGN KEY (brand_id) REFERENCES hw_erp_brands(brand_id),
+  FOREIGN KEY (equip_category_id) REFERENCES hw_erp_equipment_categories(equipment_category_id)
 );
 
-CREATE TABLE hw_erp_equipment_stock(
-    equipment_stock_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    equipment_id BIGINT NOT NULL,
-    last_maBIGINTenance_date date NOT NULL,
-    equipment_status ENUM('AVAILABLE', 'IN_USE', 'MABIGINTENANCE') NOT NULL DEFAULT 'AVAILABLE',
+CREATE TABLE hw_erp_equipment_use_report(
+    equip_report_id BIGINT AUTO_INCREMENT NOT NULL,
+    employee_id BIGINT NOT NULL,
+    equip_id BIGINT NOT NULL,
+    taken_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    return_date TIMESTAMP,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (equipment_id) REFERENCES hw_erp_equipment(equipment_id)
+    FOREIGN KEY (employee_id) REFERENCES hw_erp_employees (employee_id),
+    FOREIGN KEY (equip_id) REFERENCES hw_erp_equipment_stock (equip_id)
 );
 
-CREATE TABLE hw_erp_work_equipment_needed(
-    work_equipment_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    work_id BIGINT NOT NULL,
-    equipment_stock_id BIGINT NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (work_id) REFERENCES hw_erp_work_schedule(work_id),
-    FOREIGN KEY (equipment_stock_id) REFERENCES hw_erp_equipment_stock(equipment_stock_id)
-);
-
-
-CREATE TABLE hw_erp_fleets(
+CREATE TABLE hw_erp_fleet(
     vehicle_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    brand VARCHAR(200) NOT NULL,
+    brand_id BIGINT NOT NULL,
     model VARCHAR(200) NOT NULL,
     motor VARCHAR(200) NOT NULL,
     year BIGINT NOT NULL,
     trans_type ENUM('AUTOMATIC', 'MANUAL') NOT NULL,
-    license_plate VARCHAR(20) NOT NULL,
+    vin_number VARCHAR(20) NOT NULL,
     type ENUM('Car', 'Truck', 'Van') NOT NULL,
-    status ENUM('Available', 'In Use', 'MaBIGINTenance') NOT NULL,
-    last_maBIGINTenance_date DATE,
+    status ENUM('Available', 'In Use', 'Maintenance') NOT NULL,
+    last_maintenance_date DATE,
+    current_miles BIGINT NOT NULL,
+    subtotal DECIMAL(10,2) NOT NULL,
+    taxes DECIMAL(10,2) NOT NULL,
+    total DECIMAL(10,2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE hw_erp_maintenance_fleet(
     maintenance_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    date DATE,
+    date DATE NOT NULL,
     vehicle_id BIGINT NOT NULL,
     mile_count BIGINT NOT NULL,
     description TEXT, 
+    img_receipt VARCHAR(250) NOT NULL,
+    subtotal DECIMAL(10,2) NOT NULL,
+    taxes DECIMAL(10,2) NOT NULL,
+    total DECIMAL(10,2) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (vehicle_id) REFERENCES hw_erp_fleets(vehicle_id)
+    FOREIGN KEY (vehicle_id) REFERENCES hw_erp_fleet(vehicle_id)
 );
 
 Create TABLE hw_erp_vehicle_crew(
@@ -218,7 +222,7 @@ Create TABLE hw_erp_vehicle_crew(
     driver BIGINT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    FOREIGN KEY (vehicle_id) REFERENCES hw_erp_fleets(vehicle_id),
+    FOREIGN KEY (vehicle_id) REFERENCES hw_erp_fleet(vehicle_id),
     FOREIGN KEY (crew_id) REFERENCES hw_erp_crews(crew_id),
     FOREIGN KEY (driver_id) REFERENCES hw_erp_employees(employee_id)
 );
@@ -226,8 +230,8 @@ Create TABLE hw_erp_vehicle_crew(
 CREATE TABLE hw_erp_invoices (
     invoice_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     client_id BIGINT NOT NULL,
-    date_created DATE NOT NULL,
-    date_due DATE NOT NULL,
+    sent_date DATE NOT NULL,
+    due_date DATE NOT NULL,
     subtotal DECIMAL(10,2) NOT NULL,
     tax DECIMAL(10,2) NOT NULL,
     total DECIMAL(10,2) NOT NULL,
@@ -236,13 +240,28 @@ CREATE TABLE hw_erp_invoices (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (client_id) REFERENCES hw_erp_clients(client_id)
 );
+-- this will require a complex query when creating the total of the invoice since the invoice 
+--  can have various jobs in the invoice
+CREATE TABLE hw_erp_invoice_summary(
+  invoice_job_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  invoice_id BIGINT NOT NULL,
+  job_id BIGINT NOT NULL,
+  total DECIMAL(10,2) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (invoice_id) REFERENCES hw_erp_invoices(invoice_id),
+  FOREIGN KEY (job_id) REFERENCES hw_erp_jobs(job_id)
+);
+
 
 CREATE TABLE hw_erp_estimates (
   estimate_id BIGINT AUTO_INCREMENT PRIMARY KEY,
   client_id BIGINT NOT NULL,
-  job_id BIGINT NOT NULL,
+  estimate_tasks BIGINT NOT NULL,
   description TEXT NOT NULL,
   estimate_date DATE NOT NULL,
+  subtotal DECIMAL(10,2) NOT NULL,
+  taxes DECIMAL(10,2) NOT NULL,
   total_amount DECIMAL(10,2) NOT NULL,
   status ENUM('Pending', 'Accepted', 'Declined') NOT NULL,
   notes TEXT,
@@ -252,19 +271,61 @@ CREATE TABLE hw_erp_estimates (
   FOREIGN KEY (job_id) REFERENCES hw_erp_work_schedule(job_id)
 );
 
+CREATE TABLE hw_erp_task_estimate (
+  task_est_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  estimate_id BIGINT NOT NULL,
+  service_id BIGINT NOT NULL,
+  hours INT,
+  total DECIMAL(10,2) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (estimate_id) REFERENCES hw_erp_estimates(estimate_id),
+  FOREIGN KEY (service_id) REFERENCES hw_erp_services(service_id)
+);
+
+
+
 CREATE TABLE hw_erp_expenses (
   expense_id BIGINT AUTO_INCREMENT PRIMARY KEY,
-  crew_id BIGINT NOT NULL,
-  expense_date DATE NOT NULL,
-  expense_description VARCHAR(255) NOT NULL,
-  type VARCHAR(200) NOT NULL,
+  description VARCHAR(255) NOT NULL,
   subtotal DECIMAL(10,2) NOT NULL,
   tax DECIMAL(10,2) NOT NULL,
   total DECIMAL(10, 2) NOT NULL,
-  category VARCHAR(255) NOT NULL,
   notes VARCHAR(255),
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (crew_id) REFERENCES hw_erp_crews(crew_id)
 );
+
+CREATE TABLE hw_erp_expense_reciepts(
+  exp_reciept_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  expense_id BIGINT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (expense_id) REFERENCES hw_erp_expenses(expense_id)
+);
+
+-- this table can be used to reference items for easier future purchase
+CREATE TABLE hw_erp_expense_items(
+  item_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  expense_id BIGINT NOT NULL,
+  name VARCHAR(100) NOT NULL,
+  qty INT NOT NULL,
+  total DECIMAL(10,2) NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (expense_id) REFERENCES hw_erp_expenses(expense_id)
+);
+
+CREATE TABLE hw_erp_equipment_expenses (
+  equip_expense_id BIGINT AUTO_INCREMENT PRIMARY KEY,
+  equip_id BIGINT NOT NULL,
+  invoice_id BIGINT NOT NULL,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  FOREIGN KEY (equip_id) REFERENCES hw_erp_equipment_stock(equip_id),
+  FOREIGN KEY (invoice_id) REFERENCES hw_erp_invoices(invoice_id)
+);
+
+
+-- Need to add projects tables
 
