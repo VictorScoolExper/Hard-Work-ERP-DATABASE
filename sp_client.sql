18cad5c3c2285a9c7fb270cd1937bd1e9eeab3c5
VictorScoolExper <victorscool477@outlook.com>
2023-03-31 21:39:21 -0600

    added sp to clients

--- sp_client.sql
+++ sp_client.sql
@@ -1,25 +1,5 @@
--- add client and list of address
-CREATE PROCEDURE sp_insert_client (
-  IN p_first_name VARCHAR(100),
-  IN p_last_name VARCHAR(100),
-  IN p_email VARCHAR(255),
-  IN p_cell_number VARCHAR(11),
-  IN p_life_stage ENUM('Customer', 'Lead', 'Opportunity'),
-  IN p_address_list TEXT
-)
-BEGIN
-  DECLARE v_client_id BIGINT;
-  DECLARE v_address_id BIGINT;
-  DECLARE v_street VARCHAR(255);
-  DECLARE v_city VARCHAR(255);
-  DECLARE v_state VARCHAR(255);
-  DECLARE v_zip_code VARCHAR(255);
-  DECLARE v_country VARCHAR(255);
-  DECLARE v_address_list_length INT DEFAULT 0;
-  DECLARE i INT DEFAULT 1;
-  
-  -- Insert the client record into the clients table
-CREATE PROCEDURE sp_insert_client (
+-- Insert the client record into the clients table
+CREATE PROCEDURE sp_insert_client(
   IN p_first_name VARCHAR(100),
   IN p_last_name VARCHAR(100),
   IN p_email VARCHAR(255),
@@ -29,23 +9,27 @@ CREATE PROCEDURE sp_insert_client (
 )
 BEGIN
   DECLARE last_id BIGINT DEFAULT 0;
-
-  INSERT INTO clients (first_name, last_name, email, cell_number, life_stage)
+  DECLARE i INT DEFAULT 0;
+  DECLARE num_addresses INT DEFAULT JSON_LENGTH(p_addresses);
+  INSERT INTO clients(first_name, last_name, email, cell_number, life_stage)
   VALUES (p_first_name, p_last_name, p_email, p_cell_number, p_life_stage);
-
   SET last_id = LAST_INSERT_ID();
-
-  INSERT INTO addresses (street, city, state, zip_code, country)
-  SELECT JSON_EXTRACT(a, '$.street'), JSON_EXTRACT(a, '$.city'), JSON_EXTRACT(a, '$.state'), JSON_EXTRACT(a, '$.zip_code'), JSON_EXTRACT(a, '$.country')
-  FROM JSON_TABLE(p_addresses, '$[*]' COLUMNS (
-    a JSON PATH '$'
-  )) AS j;
-
-  INSERT INTO client_addresses (client_id, address_id)
-  SELECT last_id, address_id
-  FROM addresses
-  WHERE address_id >= LAST_INSERT_ID() - JSON_LENGTH(p_addresses);
-END;
+ 
+  SET i = 0;
+  WHILE i < num_addresses DO
+    INSERT INTO addresses(street, city, state, zip_code, country)
+    VALUES (
+      JSON_UNQUOTE(JSON_EXTRACT(p_addresses, CONCAT('$[', i, '].street'))),
+      JSON_UNQUOTE(JSON_EXTRACT(p_addresses, CONCAT('$[', i, '].city'))),
+      JSON_UNQUOTE(JSON_EXTRACT(p_addresses, CONCAT('$[', i, '].state'))),
+      JSON_UNQUOTE(JSON_EXTRACT(p_addresses, CONCAT('$[', i, '].zip_code'))),
+      JSON_UNQUOTE(JSON_EXTRACT(p_addresses, CONCAT('$[', i, '].country')))
+    );
+    INSERT INTO client_addresses(address_id, client_id)
+    VALUES (LAST_INSERT_ID(), last_id);
+    SET i = i + 1;
+  END WHILE;
+END 
 
 -- Get all clients
 CREATE PROCEDURE sp_get_all_clients()
@@ -53,15 +37,57 @@ BEGIN
   SELECT * FROM clients;
 END;
 
--- get addresses of client by id
+-- get addresses of client by client id
 CREATE PROCEDURE sp_get_client_addresses(
   IN p_client_id BIGINT
 )
 BEGIN
-  SELECT addresses.*
-  FROM client_addresses
-  JOIN addresses ON client_addresses.address_id = addresses.address_id
-  WHERE client_addresses.client_id = p_client_id;
+  SELECT a.*
+  FROM addresses a
+  INNER JOIN client_addresses ca ON a.address_id = ca.address_id
+  WHERE ca.client_id = p_client_id;
+END;
+
+-- get client by client id
+CREATE PROCEDURE sp_get_client_info (
+  IN p_client_id BIGINT
+)
+BEGIN
+  SELECT *
+  FROM clients
+  WHERE client_id = p_client_id;
+END;
+
+-- Delete address
+CREATE PROCEDURE sp_delete_address(
+  IN p_client_id BIGINT,
+  IN p_address_id BIGINT
+)
+BEGIN
+  DELETE FROM client_addresses 
+  WHERE client_id = p_client_id AND address_id = p_address_id;
+
+  DELETE FROM addresses 
+  WHERE address_id = p_address_id;
+END;
+
+-- update client
+CREATE PROCEDURE sp_update_client (
+  IN p_client_id BIGINT,
+  IN p_first_name VARCHAR(100),
+  IN p_last_name VARCHAR(100),
+  IN p_email VARCHAR(255),
+  IN p_cell_number VARCHAR(11),
+  IN p_life_stage ENUM('Customer', 'Lead', 'Opportunity')
+)
+BEGIN
+  UPDATE clients
+  SET first_name = p_first_name,
+      last_name = p_last_name,
+      email = p_email,
+      cell_number = p_cell_number,
+      life_stage = p_life_stage
+  WHERE client_id = p_client_id;
 END;
 
 
