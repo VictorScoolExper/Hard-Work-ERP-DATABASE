16cf78743cafebb3ce4b72eaee7fd4787ff1fcd8
VictorScoolExper <victorscool477@outlook.com>
2023-05-07 19:56:39 -0600

    troubleshoot insert employee sp

---
+++ hard_work_db_v_01.sql
@@ -0,0 +1,163 @@
+CREATE DATABASE hard_work_erp_db;
+USE hard_work_erp_db;
+
+CREATE TABLE `users` (
+  `user_id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `last_name` varchar(200) NOT NULL,
+  `cell_number` varchar(20) DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  `birth_date` date DEFAULT NULL,
+  `active` enum('true','false') DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_id`)
+);
+
+CREATE TABLE `auths` (
+  `user_id` bigint DEFAULT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE KEY `user_id` (`user_id`),
+  CONSTRAINT `auths_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `employees` (
+  `employee_id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` bigint NOT NULL,
+  `created_by` bigint,
+  `edited_by` bigint,
+  `job_title` varchar(100) NOT NULL,
+  `department` varchar(100) NOT NULL,
+  `driver_license` varchar(250) NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date DEFAULT NULL,
+  `wage_per_hour` decimal(10,2) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `image_name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`employee_id`),
+  UNIQUE KEY `user_id` (`user_id`),
+  -- KEY `created_by` (`created_by`),
+  -- KEY `edited_by` (`edited_by`),
+  CONSTRAINT `employees_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
+  -- CONSTRAINT `employees_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `users` (`user_id`) ON DELETE SET NULL,
+  -- CONSTRAINT `employees_ibfk_3` FOREIGN KEY (`edited_by`) REFERENCES `users` (`user_id`) ON DELETE SET NULL
+);
+
+CREATE TABLE `shifts` (
+  `shift_id` bigint NOT NULL AUTO_INCREMENT,
+  `employee_id` bigint NOT NULL,
+  `shift_date` date NOT NULL,
+  `start_time` time NOT NULL,
+  `end_time` time NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`shift_id`),
+  KEY `employee_id` (`employee_id`),
+  CONSTRAINT `shifts_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `attendances` (
+  `attendance_id` bigint NOT NULL AUTO_INCREMENT,
+  `employee_id` bigint NOT NULL,
+  `status` enum('present','absent') DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`attendance_id`),
+  KEY `employee_id` (`employee_id`),
+  CONSTRAINT `attendances_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `crews` (
+  `crew_id` bigint NOT NULL AUTO_INCREMENT,
+  `crew_name` varchar(255) NOT NULL,
+  `crew_leader` bigint DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`crew_id`),
+  KEY `crew_leader` (`crew_leader`),
+  CONSTRAINT `crews_ibfk_1` FOREIGN KEY (`crew_leader`) REFERENCES `employees` (`employee_id`) ON DELETE SET NULL
+)
+
+CREATE TABLE `crews_employees` (
+  `crew_id` bigint NOT NULL,
+  `employee_id` bigint NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY `crew_id` (`crew_id`),
+  KEY `employee_id` (`employee_id`),
+  CONSTRAINT `crews_employees_ibfk_1` FOREIGN KEY (`crew_id`) REFERENCES `crews` (`crew_id`) ON DELETE CASCADE,
+  CONSTRAINT `crews_employees_ibfk_2` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`) ON DELETE CASCADE
+)
+
+
+CREATE TABLE `clients` (
+  `client_id` bigint NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `cell_number` varchar(11) NOT NULL,
+  `life_stage` enum('customer','lead','opportunity') DEFAULT 'customer',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`client_id`)
+)
+
+CREATE TABLE `addresses` (
+  `address_id` bigint NOT NULL AUTO_INCREMENT,
+  `street` varchar(255) NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `zip_code` varchar(255) NOT NULL,
+  `country` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`address_id`)
+)
+
+CREATE TABLE `client_addresses` (
+  `address_id` bigint NOT NULL,
+  `client_id` bigint NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  UNIQUE KEY `address_id` (`address_id`),
+  KEY `client_id` (`client_id`),
+  CONSTRAINT `client_addresses_ibfk_1` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`address_id`) ON DELETE CASCADE,
+  CONSTRAINT `client_addresses_ibfk_2` FOREIGN KEY (`client_id`) REFERENCES `clients` (`client_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `companies` (
+  `company_id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`company_id`)
+)
+
+CREATE TABLE `vendors` (
+  `vendor_id` bigint NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(100) NOT NULL,
+  `last_name` varchar(100) DEFAULT NULL,
+  `company_id` bigint NOT NULL,
+  `cell_number` varchar(20) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`vendor_id`),
+  KEY `company_id` (`company_id`),
+  CONSTRAINT `vendors_ibfk_1` FOREIGN KEY (`company_id`) REFERENCES `companies` (`company_id`) ON DELETE CASCADE
+);
+
+CREATE TABLE `vendor_addresses` (
+  `address_id` bigint NOT NULL,
+  `vendor_id` bigint NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  KEY `address_id` (`address_id`),
+  KEY `vendor_id` (`vendor_id`),
+  CONSTRAINT `vendor_addresses_ibfk_1` FOREIGN KEY (`address_id`) REFERENCES `addresses` (`address_id`) ON DELETE CASCADE,
+  CONSTRAINT `vendor_addresses_ibfk_2` FOREIGN KEY (`vendor_id`) REFERENCES `vendors` (`vendor_id`) ON DELETE CASCADE
+)
