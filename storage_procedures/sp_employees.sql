c95562fd23aba58e95c68a1d30d42f1666f36b65
VictorScoolExper <victorscool477@outlook.com>
2023-05-07 19:53:19 -0600

    troubleshoot insert employee sp

--- storage_procedures/sp_employees.sql
+++ storage_procedures/sp_employees.sql
@@ -4,7 +4,8 @@ CREATE PROCEDURE sp_insert_employee(
     IN p_last_name VARCHAR(200),
     IN p_cell_number VARCHAR(20),
     IN p_role VARCHAR(20),
-    IN p_age INT,
+    IN p_birth_date DATE,
+    IN p_image_name VARCHAR
     IN p_job_title VARCHAR(100),
     IN p_department VARCHAR(100),
     IN p_driver_license VARCHAR(250),
@@ -16,25 +17,24 @@ BEGIN
     DECLARE p_user_id BIGINT;
 
     -- Insert user record first
-    INSERT INTO users (name, last_name, cell_number, role, age, active)
-    VALUES (p_name, p_last_name, p_cell_number, p_role, p_age, "true");
+    INSERT INTO users (name, last_name, cell_number, role, birth_date, active)
+    VALUES (p_name, p_last_name, p_cell_number, p_role, p_birth_date, "true");
 
     -- Get the user_id of the newly created user
     SET p_user_id = LAST_INSERT_ID();
 
     -- Insert employee record
-    INSERT INTO employees (user_id, created_by, edited_by, job_title, department, driver_license, start_date, wage_per_hour)
-    VALUES (p_user_id, p_created_by_user, p_created_by_user, p_job_title, p_department, p_driver_license, p_start_date, p_wage_per_hour);
+    INSERT INTO employees (user_id, created_by, edited_by, job_title, department, driver_license, start_date, wage_per_hour, image_name)
+    VALUES (p_user_id, p_created_by_user, p_created_by_user, p_job_title, p_department, p_driver_license, p_start_date, p_wage_per_hour, p_image_name);
 END
 
 -- Get all Employees active or inactive
-CREATE PROCEDURE get_employee_list(IN is_active)
+CREATE PROCEDURE get_employee_list()
 BEGIN
-    SELECT u.name, u.last_name, u.cell_number, u.role, u.age, e.job_title, e.department, e.driver_license, e.start_date, e.end_date, e.wage_per_hour
+    SELECT u.name, u.last_name, u.cell_number, u.role, u.birth_date, e.job_title, e.department, e.driver_license, e.start_date, e.end_date, e.wage_per_hour
     FROM users u
     INNER JOIN employees e
-    ON u.user_id = e.user_id
-    WHERE u.active = is_active;
+    ON u.user_id = e.user_id;
 END;
 
 
