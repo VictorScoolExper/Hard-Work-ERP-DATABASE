db10d5819a8412ee555ebc0370fcd8a22236700f
VictorScoolExper <victorscool477@outlook.com>
2023-05-31 18:01:47 -0600

    modified sp materials

--- Storage Procedures/sp_materials.sql
+++ Storage Procedures/sp_materials.sql
@@ -1,40 +1,37 @@
--- CREATE TABLE `materials` (
---   `material_id` int NOT NULL AUTO_INCREMENT,
---   `material_name` varchar(100) NOT NULL,
---   `description` varchar(255),
---   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
---   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
---   PRIMARY KEY (`material_id`)
--- )
-
+DROP PROCEDURE IF EXISTS sp_create_materials;
 CREATE PROCEDURE sp_create_materials (
     IN p_material_name VARCHAR(100),
-    IN p_description VARCHAR(255)
+    IN p_description VARCHAR(255),
+    IN p_unit enum('yard','ton','piece')
 )
 BEGIN
     START TRANSACTION;
-        INSERT INTO materials (material_name, description)
-        VALUES (p_material_name, p_description);
+        INSERT INTO materials (material_name, description, unit)
+        VALUES (p_material_name, p_description, p_unit);
     COMMIT;
 END
 
+DROP PROCEDURE IF EXISTS sp_get_materials;
 CREATE PROCEDURE sp_get_materials ()
 BEGIN
     START TRANSACTION;
-        SELECT material_id, material_name, description FROM materials;
+        SELECT material_id, material_name, description, unit FROM materials;
     COMMIT;
 END
 
+DROP PROCEDURE IF EXISTS sp_update_material;
 CREATE PROCEDURE sp_update_material(
     IN p_material_id INT,
     IN p_material_name VARCHAR(100),
-    IN p_description VARCHAR(255)
+    IN p_description VARCHAR(255),
+    IN p_unit enum('yard','ton','piece')
 )
 BEGIN
     START TRANSACTION;
         UPDATE materials
         SET material_name = p_material_name,
-            description = p_description
+            description = p_description,
+            unit = p_unit
         WHERE material_id = p_material_id;
     COMMIT;
 END
