77f18715d00b808b409f7dc92c3a05acaa688b6a
VictorScoolExper <victorscool477@outlook.com>
2023-06-21 20:03:22 -0600

    added to while loop throught service JSON

--- Storage Procedures/sp_schedule.sql
+++ Storage Procedures/sp_schedule.sql
@@ -13,7 +13,11 @@ CREATE PROCEDURE SP_CREATE_SERVICE_SCHEDULE(
     IN p_employees JSON, 
    
 ) BEGIN 
+    DECLARE currentIndex INT DEFAULT 0;
+    DECLARE totalElements INT;
+    DECLARE currentElement JSON;
 	DECLARE last_id BIGINT DEFAULT 0;
+
 	START TRANSACTION;
         -- TODO: insert into service schedule
         INSERT INTO service_schedule (
@@ -38,22 +42,56 @@ CREATE PROCEDURE SP_CREATE_SERVICE_SCHEDULE(
         -- set last_id variable with last id
         SET last_id = LAST_INSERT_ID();
 
+        SET totalElements = JSON_LENGTH(p_services)
         -- validate that services exist
         IF JSON_LENGTH(p_services) > 0 THEN 
-            -- TODO: INSERT STATEMENT
-            INSERT INTO 
-        ELSE SIGNAL SQLSTATE '45000'
-        SET
+            WHILE currentIndex < totalElements DO
+                SET currentElement = JSON_EXTRACT(p_services, CONCAT('$[', currentIndex, ']'));
+
+                -- Process current json
+                DECLARE p_service_schedule_id INT;
+                DECLARE p_service_id INT;
+                DECLARE p_quantity INT;
+
+                SET p_service_schedule_id = JSON_UNQUOTE(JSON_EXTRACT(currentElement, '$.client_schedule_id'));
+                SET p_service_id = JSON_UNQUOTE(JSON_EXTRACT(currentElement, '$.service_id'));
+                SET p_quantity = JSON_UNQUOTE(JSON_EXTRACT(currentElement, '.$quantity'));
+
+                -- INSERT STATEMENT
+                INSERT INTO scheduled_services (
+                    client_schedule, 
+                    service_id, 
+                    quantity
+                ) 
+                VALUES (
+                    p_service_schedule_id,
+                    p_service_id,
+                    p_quantity
+                );
+                -- set the next index
+                SET currentIndex = currentIndex + 1;
+            END WHILE;
+        ELSE 
+            SIGNAL SQLSTATE '45000'
+            SET
             MESSAGE_TEXT = 'No services provided'
         END IF;
 
+
+        SET totalElements = JSON_LENGTH(p_materials);
+        SET currentIndex = 0;
+
         -- validate that materials exist
         IF JSON_LENGTH(p_materials) > 0 THEN 
-            -- TODO: INSERT STATEMENT
-            
-        ELSE SIGNAL SQLSTATE '45000'
-        SET
-            MESSAGE_TEXT = 'No materials provided'
+            WHILE currentIndex < totalElements DO
+                SET currentElement = JSON_EXTRACT(jsonArray, CONCAT('$[', currentIndex, ']'));
+                
+                -- Process current json
+                DECLARE p_client_service_ide
+                -- TODO: INSERT STATEMENT
+                
+                SET currentIndex = currentIndex + 1;
+            END WHILE;
         END IF;
 
         -- validate if employees exist
