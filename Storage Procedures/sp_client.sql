4438e792fa292c908826002dd2e0082a16214609
VictorScoolExper <victorscool477@outlook.com>
2023-06-21 18:58:45 -0600

    added sp_create_schedule

--- Storage Procedures/sp_client.sql
+++ Storage Procedures/sp_client.sql
@@ -1,109 +1,129 @@
 -- Insert the client record into the clients table
-CREATE PROCEDURE sp_insert_vendor(
-  IN p_name VARCHAR(100),
-  IN p_last_name VARCHAR(100),
-  IN p_company_id BIGINT,
-  IN p_cell_number VARCHAR(20),
-  IN p_email VARCHAR(255),
-  IN p_street VARCHAR(255),
-  IN p_city VARCHAR(100),
-  IN p_state VARCHAR(100),
-  IN p_zip_code VARCHAR(20),
-  IN p_country VARCHAR(10)
-)
-BEGIN
-  DECLARE last_id BIGINT DEFAULT 0;
-  START TRANSACTION;
-    INSERT
-	INTO
-	vendors(name,
-	last_name,
-	company_id,
-	cell_number,
-	email)
-VALUES (p_name,
-p_last_name,
-p_company_id,
-p_cell_number,
-p_email);
---    save for vendor_address insert
-    SET last_id = LAST_INSERT_ID();
 
-    INSERT INTO addresses(street, city, state, zip_code, country)
-    VALUES (p_street, p_city, p_state, p_zip_code, p_country);
-   
-    INSERT INTO vendor_addresses (address_id, vendor_id)
-    VALUES (LAST_INSERT_ID(), last_id);
-  COMMIT;
-END;
+CREATE PROCEDURE SP_INSERT_VENDOR(IN P_NAME VARCHAR
+(100), IN P_LAST_NAME VARCHAR(100), IN P_COMPANY_ID 
+BIGINT, IN P_CELL_NUMBER VARCHAR(20), IN P_EMAIL VARCHAR
+(255), IN P_STREET VARCHAR(255), IN P_CITY VARCHAR
+(100), IN P_STATE VARCHAR(100), IN P_ZIP_CODE VARCHAR
+(20), IN P_COUNTRY VARCHAR(10)) BEGIN 
+	DECLARE last_id BIGINT DEFAULT 0;
+	START TRANSACTION;
+	INSERT INTO
+	    vendors(
+	        name,
+	        last_name,
+	        company_id,
+	        cell_number,
+	        email
+	    )
+	VALUES (
+	        p_name,
+	        p_last_name,
+	        p_company_id,
+	        p_cell_number,
+	        p_email
+	    );
+	--    save for vendor_address insert
+	SET last_id = LAST_INSERT_ID();
+	INSERT INTO
+	    addresses(
+	        street,
+	        city,
+	        state,
+	        zip_code,
+	        country
+	    )
+	VALUES (
+	        p_street,
+	        p_city,
+	        p_state,
+	        p_zip_code,
+	        p_country
+	    );
+	INSERT INTO
+	    vendor_addresses (address_id, vendor_id)
+	VALUES (LAST_INSERT_ID(), last_id);
+	COMMIT;
+END; 
 
 -- Get all clients
-CREATE PROCEDURE sp_get_clients()
-BEGIN
-  -- select that fields we will work with
-  SELECT client_id, name, last_name, email, cell_number, life_stage FROM clients;
-END;
+
+CREATE PROCEDURE SP_GET_CLIENTS() BEGIN 
+	-- select that fields we will work with
+	SELECT
+	    client_id,
+	    name,
+	    last_name,
+	    email,
+	    cell_number,
+	    life_stage
+	FROM clients;
+END; 
 
 -- get addresses of client by client id
-CREATE PROCEDURE sp_get_client_addresses(
-  IN p_client_id BIGINT
-)
-BEGIN
-  SELECT 
-	  a.address_id, a.street, a.city, a.state, a.zip_code 
-  FROM addresses AS a 
-  INNER JOIN client_addresses AS ca ON a.address_id = ca.address_id
-  WHERE ca.client_id = p_client_id
-  ORDER BY a.created_at DESC;
-END;
+
+CREATE PROCEDURE SP_GET_CLIENT_ADDRESSES(IN P_CLIENT_ID 
+BIGINT) BEGIN 
+	SELECT
+	    a.address_id,
+	    a.street,
+	    a.city,
+	    a.state,
+	    a.zip_code
+	FROM addresses AS a
+	    INNER JOIN client_addresses AS ca ON a.address_id = ca.address_id
+	WHERE
+	    ca.client_id = p_client_id
+	ORDER BY a.created_at DESC;
+END; 
 
 -- get client by client id
-CREATE PROCEDURE sp_get_client_by_id (
-  IN p_client_id BIGINT
-)
-BEGIN
-  SELECT c.client_id, c.name, c.last_name, c.email, c.cell_number, c.life_stage
-  FROM clients AS c
-  WHERE c.client_id = p_client_id;
-END;
+
+CREATE PROCEDURE SP_GET_CLIENT_BY_ID(IN P_CLIENT_ID 
+BIGINT) BEGIN 
+	SELECT
+	    c.client_id,
+	    c.name,
+	    c.last_name,
+	    c.email,
+	    c.cell_number,
+	    c.life_stage
+	FROM clients AS c
+	WHERE
+	    c.client_id = p_client_id;
+END; 
 
 -- Delete address
-CREATE PROCEDURE sp_delete_address(
-  IN p_client_id BIGINT,
-  IN p_address_id BIGINT
-)
-BEGIN
-  START TRANSACTION;
-  DELETE FROM client_addresses 
-  WHERE client_id = p_client_id AND address_id = p_address_id;
 
-  DELETE FROM addresses 
-  WHERE address_id = p_address_id;
-  COMMIT;
-END;
+CREATE PROCEDURE SP_DELETE_ADDRESS(IN P_CLIENT_ID BIGINT
+, IN P_ADDRESS_ID BIGINT) BEGIN 
+	START TRANSACTION;
+	DELETE FROM client_addresses
+	WHERE
+	    client_id = p_client_id
+	    AND address_id = p_address_id;
+	DELETE FROM addresses WHERE address_id = p_address_id;
+	COMMIT;
+END; 
 
 -- update client
-CREATE PROCEDURE sp_update_client (
-  IN p_client_id BIGINT,
-  IN p_name VARCHAR(100),
-  IN p_last_name VARCHAR(100),
-  IN p_email VARCHAR(255),
-  IN p_cell_number VARCHAR(20),
-  IN p_life_stage ENUM('customer', 'lead', 'opportunity')
-)
-BEGIN
-  START TRANSACTION;
-    UPDATE clients
-    SET name = p_name,
-        last_name = p_last_name,
-        email = p_email,
-        cell_number = p_cell_number,
-        life_stage = p_life_stage
-    WHERE client_id = p_client_id;
-  COMMIT;
-END;
+
+CREATE PROCEDURE SP_UPDATE_CLIENT(IN P_CLIENT_ID BIGINT
+, IN P_NAME VARCHAR(100), IN P_LAST_NAME VARCHAR(100
+), IN P_EMAIL VARCHAR(255), IN P_CELL_NUMBER VARCHAR
+(20), IN P_LIFE_STAGE ENUM('CUSTOMER', 'LEAD', 'OPPORTUNITY'
+)) BEGIN 
+	START TRANSACTION;
+	UPDATE clients
+	SET
+	    name = p_name,
+	    last_name = p_last_name,
+	    email = p_email,
+	    cell_number = p_cell_number,
+	    life_stage = p_life_stage
+	WHERE
+	    client_id = p_client_id;
+	COMMIT;
+END; 
 
 -- If you are looking for the update address for client checkout the sp_address file
-
-
-
--- Storage Procedures/sp_schedule.sql
+++ Storage Procedures/sp_schedule.sql
@@ -1,33 +1,65 @@
 DROP PROCEDURE IF EXISTS sp_create_service_schedule;
-CREATE PROCEDURE sp_create_service_schedule (
-    IN p_client_id INT,
-    IN p_address_id INT,
-    IN p_services JSON,
-    IN p_materials JSON,
-    IN p_employees JSON,
-    IN p_start_time date,
-    IN p_to_do_date date
-)
-BEGIN
-    START TRANSACTION;
+
+CREATE PROCEDURE SP_CREATE_SERVICE_SCHEDULE(
+    IN p_client_id INT, 
+    IN p_address_id INT, 
+    IN p_start_time DATE, 
+    IN p_end_time DATE,
+    IN p_to_do_date DATE,
+    IN p_type enum('single', 'routine'),
+    IN p_status enum('pending', 'in-progress', 'done', 'canceled')
+    IN p_services JSON, 
+    IN p_materials JSON, 
+    IN p_employees JSON, 
+   
+) BEGIN 
+	DECLARE last_id BIGINT DEFAULT 0;
+	START TRANSACTION;
         -- TODO: insert into service schedule
+        INSERT INTO service_schedule (
+            client_id,
+            address_id,
+            start_time,
+            end_time,
+            to_do_date,
+            type,
+            status
+        )
+        VALUES (
+            p_client_id, 
+            p_services, 
+            p_address_id, 
+            p_start_time, 
+            p_end_time, 
+            p_to_do_date,
+            p_type,
+            p_status
+        );
+        -- set last_id variable with last id
+        SET last_id = LAST_INSERT_ID();
 
-        IF JSON_LENGTH(p_services) > 0 THEN
+        -- validate that services exist
+        IF JSON_LENGTH(p_services) > 0 THEN 
             -- TODO: INSERT STATEMENT
-        ELSE 
-            SIGNAL SQLSTATE  '45000' SET MESSAGE_TEXT = 'No services provided'
+            INSERT INTO 
+        ELSE SIGNAL SQLSTATE '45000'
+        SET
+            MESSAGE_TEXT = 'No services provided'
         END IF;
 
-        IF JSON_LENGTH(p_materials) > 0 THEN
+        -- validate that materials exist
+        IF JSON_LENGTH(p_materials) > 0 THEN 
             -- TODO: INSERT STATEMENT
-        ELSE 
-            SIGNAL SQLSTATE  '45000' SET MESSAGE_TEXT = 'No materials provided'
+            
+        ELSE SIGNAL SQLSTATE '45000'
+        SET
+            MESSAGE_TEXT = 'No materials provided'
         END IF;
 
-        IF JSON_LENGTH(p_employees) > 0 THEN
+        -- validate if employees exist
+        IF JSON_LENGTH(p_employees) > 0 THEN 
             -- TODO: INSERT STATEMENT
         END IF;
 
-
-    COMMIT;
-END
+	COMMIT;
+	END 
