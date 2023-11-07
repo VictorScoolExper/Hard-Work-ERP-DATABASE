e62c57d7e878c704d4db7b3b81487a81ebb9475b
VictorScoolExper <victorscool477@outlook.com>
2023-11-06 20:38:12 -0600

    added data script

---
+++ data.sql
@@ -0,0 +1,3 @@
+INSERT INTO company_roles (name, description) VALUES ('admin', 'This role gives all permissions to user');
+INSERT INTO company_roles (name, description) VALUES ('employee', 'This role is given to general employees that have no administrative access');
+
